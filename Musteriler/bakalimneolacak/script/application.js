32d55eab1b1b7e9232bf17b6643dc9824e33a24f
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-01-05 10:21:53 +0200

    1- bakalım nolcak kafes fırın dosyaları silindi 2- internet checker javascript olarak ayrıldı 3- nointernet.html sayfası internet olmaması durumu için oluşturuldu (bunu projeye de ekleyelim)

--- Musteriler/bakalimneolacak/script/application.js
+++ Musteriler/bakalimneolacak/script/application.js
@@ -1,86 +1,16 @@
-﻿//pages required to loop, 10min = 600000 milliseconds
-// example element : {url:"ekranlarge.html", duration:600000 }
-var pages = [
-];
-
-/*VIDEO TIMER FUNCTIONS AND CLASSES.*/
-/*Excluded time class to set in what time video rotation will not happen.*/
-var ExcludedTimeInterval = function(from, to)
-{
-	if(from.getTime() > to.getTime())
-	{
-		var temp = to;
-		to=from;
-		from=temp;
-	}
-	
-	this.from = from;
-	this.to = to;
-	
-	this.checkTimeIfExcluded = function(givenTime)
-							   {
-									return from.getTime() < givenTime.getTime() && to.getTime() > givenTime.getTime(); 
-							   }
-}
-
-var VideoTimer = function(){
-	if(pages.length === 0)
-	{
-		return false;
-	}
-	
-	var pageIndex = 0;
-	var excludedTimeInterval;
-	this.openNextPage = function()
-	{
-		document.getElementById("applicationframe").src = pages[pageIndex].url;
-		self.setVideoTimer(self.excludedTimeInterval);
-	}
-
-	this.setVideoTimer=function(excludedTimeInterval)
-	{
-		self.excludedTimeInterval = excludedTimeInterval;
-		var rotateInterval = pages[pageIndex].duration;
-		var presentTime = new Date();
-		var futureTime = new Date(2500, 1, 1, presentTime.getHours(), presentTime.getMinutes(), presentTime.getSeconds(), 0);
-		var totalRotateInterval = 0;
-		
-		pageIndex = (pageIndex + 1) % pages.length;
-		if(!pages[pageIndex].url.indexOf("video") == -1)
-		{
-			setTimeout(function()
-				{
-					self.openNextPage();
-				}, rotateInterval);
-		}
-		
-		do
-		{
-			totalRotateInterval += rotateInterval;
-			futureTime.setMilliseconds(futureTime.getMilliseconds() + rotateInterval);
-			var isTimeExcluded = excludedTimeInterval.checkTimeIfExcluded(futureTime);
-		}while(isTimeExcluded);
-		
-		setTimeout(function()
-		{
-			self.openNextPage();
-		},totalRotateInterval);
-	}
-	
-	//// Javascript convention to call methods in object.
-	var self = this;
-}
-
-/*APPLICATION FUNCTIONS AND CLASSES.*/
+﻿/*APPLICATION FUNCTIONS AND CLASSES.*/
 var startApp = function()
 {
-	var videoTimer = new VideoTimer();
-	var excludedFrom = new Date(2500, 1, 1, 12, 0, 0, 0);
-	var excludedTo = new Date(2500, 1, 1, 14, 0, 0, 0);
-	var excludedTimeInterval = new ExcludedTimeInterval(excludedFrom,excludedTo);
-	videoTimer.setVideoTimer(excludedTimeInterval);
+	var backupPage = "nointernet.html";
+	var internetPagesArray = [{id : "applicationframe", url : "ekrand.html?twitterId=666636527968088064&twitterName=kafesfirin"}];
+	var internetChecker = new InternetChecker(backupPage,internetPagesArray);
+	internetChecker.start(5000);
 	var logger = new Logger();
+	logger.log("Application started","application.html");
 	logger.startDiagnostics(5);
+	var errorHandler = new ErrorHandler();
+	errorHandler.start();
+	var internetChecker = new InternetChecker()
 }
 
 startApp();
