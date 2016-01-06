c7f9ef346cd6d9a50284d08c3558b3a2421d9227
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-01-06 13:03:51 +0200

    startapp script düzeltildi

--- Musteriler/kafesfirin/deployment/startapp.sh
+++ Musteriler/kafesfirin/deployment/startapp.sh
@@ -25,7 +25,7 @@ unclutter -idle 0.01 -root &
 xset -dpms &
 xset s noblank &
 xset s off &
-cd /home/bilimtek/Documents/Eyelogic/EyelogicServiceSoftware/Release/;./EyelogicServiceSoftware_1.1 &
+cd /home/bilimtek/Documents/Eyelogic/EyelogicServiceSoftware/Release/; ./EyelogicServiceSoftware_1.1 &
 sudo nw /home/homis/
 
 
