6b1af095938491c3b1ab07dede82b2b2d75ca2e6
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-01-06 12:49:06 +0200

    startapp.sh unix windows problemi

---
+++ Musteriler/kafesfirin/deployment/startapp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+start=21:10
+end=06:50
+
+checkIfWithinRange() {
+  current=$(date +%H:%M)
+  startinseconds=`date +%s -d "$start"`
+  endinseconds=`date +%s -d "$end"`
+  currentinseconds=`date +%s -d "$current"`
+  if [ "$endinseconds" -lt "$startinseconds" ]; then
+    endinseconds=$((`date +%s -d "$end"` + 24*60*60))
+  fi
+  if [ "$currentinseconds" -lt "$startinseconds" ]; then
+    currentinseconds=$((`date +%s -d "$current"` + 24*60*60))
+  fi
+  [[ ($startinsecond -eq $currentinseconds || $startinseconds -lt $currentinseconds) && ($currentinseconds -eq $endinseconds || $currentinseconds -lt $endinseconds) ]] 
+}
+
+if checkIfWithinRange; then
+  echo "going back to sleep"
+  bash /home/homis/deployment/suspenduntil.sh "$end"
+fi
+
+unclutter -idle 0.01 -root &
+xset -dpms &
+xset s noblank &
+xset s off &
+cd /home/bilimtek/Documents/Eyelogic/EyelogicServiceSoftware/Release/;./EyelogicServiceSoftware_1.1 &
+sudo nw /home/homis/
+
+
