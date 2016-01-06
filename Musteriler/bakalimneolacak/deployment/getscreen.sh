59a818132ab98b22afdf0c502f60810b02b91f29
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-01-06 11:50:28 +0200

    raspi screenshot alma scripti modifiye edildi.

--- Musteriler/bakalimneolacak/deployment/getscreen.sh
+++ Musteriler/bakalimneolacak/deployment/getscreen.sh
@@ -3,4 +3,4 @@ now=$(date +"%Y.%m.%d.%H.%M.%S");
 filenamedate="screen.$now.jpg";
 filenameuniqueid="$(cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2 | cut -d ' ' -f 2)"
 filename="$filenameuniqueid.$filenamedate";
-DISPLAY=":0.0"; export DISPLAY; import -window root -resize 600 /home/homis/log/"$filename"
+DISPLAY=:0.0; export DISPLAY; import -window root -resize 600 /home/homis/log/"$filename"
