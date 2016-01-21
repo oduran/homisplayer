1cd99203c0fa170d2f10a80f6952c0a7e76d6c57
Oguzhan Duran <oguzhan@bilimtek.com>
2016-01-21 14:08:20 +0200

    raspberry application tekilleştirilme initial commit

---
+++ Musteriler/raspberry/application/script/application.js
@@ -0,0 +1,16 @@
+﻿/*APPLICATION FUNCTIONS AND CLASSES.*/
+var startApp = function()
+{
+	var backupPage = "nointernet.html";
+	var internetPagesArray = [{id : "applicationframe", url : "ekrand.html?twitterId=689738719436365825&twitterName=simitcidnys&swarmVenueId=56937639498e8777fbd7dcd3&swarmOauthToken=A32RT2V1XZ51X4WNK324FYUHNG3H4XNBHROINKMNM4NNJ0MI"}];
+	var internetChecker = new InternetChecker(backupPage,internetPagesArray);
+	internetChecker.start(5000);
+	var logger = new Logger();
+	logger.log("Application started","application.html");
+	logger.startDiagnostics(5);
+	var errorHandler = new ErrorHandler();
+	errorHandler.start();
+	var internetChecker = new InternetChecker()
+}
+
+startApp();
---
+++ Musteriler/raspberry/application/script/errorhandler.js
@@ -0,0 +1,25 @@
+﻿//Handles the errors that is thrown to nodejs directly
+//Logs the error if logger.js is included and Logger class is defined
+var ErrorHandler = function()
+{
+   this.start = function()
+   {
+		if(typeof(process)=== 'undefined')
+		{
+		   return false;
+		}
+		process.on('uncaughtException', 
+			function(e) {
+				if(typeof(Logger) === 'undefined')
+				{
+					console.log("hata:"+e.toString());
+				}
+				else{
+					console.log("hata:"+e.toString());
+					var logger = new Logger();
+					logger.log("Unhandled exception => "+e.toString()+" => "+e.stack,"noFile");
+				}
+			}
+		);
+   }
+}
