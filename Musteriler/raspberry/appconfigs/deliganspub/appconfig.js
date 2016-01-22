a7d8d86af44f06852bfd09a6cb2d146ae6f58e6a
Oguzhan Duran <oguzhan@bilimtek.com>
2016-01-22 16:04:22 +0200

    deliganspub simitcidunyasi ve bakalım_nolcak appconfig dosyaları düzenlendi. instagram sliderında css de değişiklik yapıldı

--- Musteriler/raspberry/appconfigs/deliganspub/appconfig.js
+++ Musteriler/raspberry/appconfigs/deliganspub/appconfig.js
@@ -2,7 +2,7 @@
 {
 	twitterId:"689785930517970944",
 	twitterName:"deliganspub",
-	sliderMedia:"twitter",
+	sliderMedia:"instagram",
 	swarmVenueId:"56937639498e8777fbd7dcd3",
 	swarmOauthToken:"A32RT2V1XZ51X4WNK324FYUHNG3H4XNBHROINKMNM4NNJ0MI"
 }
--- Musteriler/raspberry/appconfigs/simitcidunyasi/appconfig.js
+++ Musteriler/raspberry/appconfigs/simitcidunyasi/appconfig.js
@@ -1,8 +1,8 @@
 ﻿var appConfig = 
 {
 	twitterId:"689738719436365825",
-	twitterName:"simitcidnys",
-	sliderMedia:"twitter",
+	twitterName:"simitcidunyasi",
+	sliderMedia:"instagram",
 	swarmVenueId:"A32RT2V1XZ51X4WNK324FYUHNG3H4XNBHROINKMNM4NNJ0MI",
 	swarmOauthToken:"56937639498e8777fbd7dcd3"
 }
