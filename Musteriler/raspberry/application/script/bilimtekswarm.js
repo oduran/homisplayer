a50f6f402551bad44fd8d6115d46dcc7c1ca8117
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-01-27 17:04:30 +0200

    1- Swarm javascript dosyaları değiştirildi.

--- Musteriler/raspberry/application/script/bilimtekswarm.js
+++ Musteriler/raspberry/application/script/bilimtekswarm.js
@@ -1,14 +1,11 @@
 var BilimtekSwarm=function(token,venueId){
-
-	this.checkinId = [];
-	this.userNames = [];
-	this.userPhotos = [];
-	this.currentMayor=[];
-	this.count=0;
+	this.people=[];
+	this.count=-1;
+	
 	var getUrl=function(){
-		console.log("getUrl");
 		return "https://api.foursquare.com/v2/venues/"+venueId+"/herenow?v=20131016&oauth_token="+token+"&"+getRandomNumber();
 	}
+	
 	var getRandomNumber=function(){
 		var date = new Date();
 		return date.getDate()+date.getHours()+date.getMinutes()+date.getSeconds();
@@ -19,72 +16,118 @@ var BilimtekSwarm=function(token,venueId){
 			url: getUrl(),
 			context: document.body
 		});
+		
 		request.success(function (data) {
-				$.each(data.response.hereNow.items, function (i, item) {
-					debugger;
-					if(self.checkinId.length>data.response.hereNow.items.length){
-						while(self.checkinId.length > 0) {
-							self.checkinId.pop();
-						}
-						while(self.currentMayor.length > 0) {
-							self.currentMayor.pop();
-						}
+				var newItems = data.response.hereNow.items;
+				if ($("div").hasClass("marqueeZeroCount")) 
+				{
+					$("#foursquare").empty();
+				}
+
+				for(var i = 0; i< self.people.length; i++)
+				{
+					if(!self.getPersonFromArray(self.people[i], newItems))
+					{
+						self.people = self.removeFromArray(self.people[i], self.people);
 					}
-					
-					if(self.checkinId.length<data.response.hereNow.items.length){
-					 i=self.checkinId.length;
+				}
+				
+				for(var i =0;i<newItems.length;i++)
+				{
+					if(!self.getPersonFromArray(newItems[i], self.people))
+					{
+						self.people.push(self.createPerson(newItems[i]));
 					}
-					
-					if(self.checkinId[i]!=data.response.hereNow.items[i].id)
-					{	 
-						if ($("div").hasClass("marqueeZeroCount")) 
-						{
-							$("#foursquare").empty();
-						}
-						var photoSuffix = data.response.hereNow.items[i].user.photo.suffix,
-						photoPrefix = data.response.hereNow.items[i].user.photo.prefix,
-						photoUrl= photoPrefix+"original"+photoSuffix,
-						firstName = data.response.hereNow.items[i].user.firstName,
-						lastName = data.response.hereNow.items[i].user.lastName;
-							if (typeof lastName === "undefined") {
-								lastName="";
-							}					
-						self.checkinId.push(data.response.hereNow.items[i].id);
-						self.currentMayor.push('<img class="marqueeleftThenRight" src="' + photoUrl + '"  width="55" height="55" /><p class="marqueebottomThenTop">Hoşgeldiniz </p><p class="marqueebottomThenTop">' + firstName+' '+lastName+ ' </p>')
-					}
-				});
-				if(data.response.hereNow.items.length==0)
+				}
+
+				if(data.response.hereNow.items.length === 0)
 				{	
 						$("#foursquare").empty();
-						while(self.checkinId.length > 0) {
-							self.checkinId.pop();
-						}
-						while(self.currentMayor.length > 0) {
-							self.currentMayor.pop();
-						}
+						self.people = [];
 						$("<div class='marqueeZeroCount'>Buraya Check-In yap!</div>").prependTo("#foursquare");
 				}					
-
 		});    
 			
 		request.fail(function () {
-			$("#foursquare").empty();
+
 			var logger = new Logger();
 			logger.log("Swarm Request Exceeded","ekrand.html");
 		});
 	}
 	
+	this.getPersonFromArray = function(item,itemArray)
+	{
+		for(var i = 0;i<itemArray.length;i++)
+		{
+			if(itemArray[i].id === item.id)
+			{
+				return itemArray[i];
+			}
+		}
+		
+		return null;
+	}
+	
+	this.removeFromArray = function(item,itemArray)
+	{
+		var resultArray = [];
+		for(var i = 0; i < itemArray.length;i++)
+		{
+			if(item.id !== itemArray[i].id)
+			{
+				resultArray.push(itemArray[i]);
+			}
+		}
+		
+		return resultArray;
+	}
+	
+	this.createPerson = function(item)
+	{
+		var photoSuffix = item.user.photo.suffix,
+		photoPrefix = item.user.photo.prefix,
+		photoUrl= photoPrefix + "original" + photoSuffix,
+		firstName = item.user.firstName;
+		lastName = (typeof item.user.lastName === "undefined")? "" : item.user.lastName;
+		var person = {
+			uiElement: '<img class="marqueeleftThenRight" src="' + photoUrl + '"  width="55" height="55" /><p class="marqueebottomThenTop">Hoşgeldiniz </p><p class="marqueebottomThenTop">' + firstName+' '+lastName+ ' </p>', 
+			id: item.id,
+			shown: false};
+		return person;
+	}
+	
 	this.iteratePeople = function(){
 		var userSlideInterval = setInterval(function(){
-			if(self.currentMayor.length>0){
-				$("#foursquare").empty();
-				$(self.currentMayor[self.count]).appendTo("#foursquare"); 
-				self.count++;
-				if(self.count==self.currentMayor.length){
-				self.count=0;
+			self.people = self.resetIfAllShown(self.people);
+			for(var i=0;i<self.people.length;i++)
+			{
+				if(!self.people[i].shown)
+				{
+					$("#foursquare").empty();
+					$(self.people[i].uiElement).appendTo("#foursquare");
+					self.people[i].shown = true;
+					return;						
 				}
 			}
 		},10000);
+	}
+	
+	this.resetIfAllShown = function(people)
+	{
+		for(var i = 0; i< people.length; i++)
+		{
+			if(!people[i].shown)
+			{
+				return people;
+			}
+		}
+		
+		for(var i = 0; i< people.length; i++)
+		{
+			people[i].shown = false;
+		}
+		
+		return people;
 	}
 	
 	this.getFoursquareData = function()
@@ -96,5 +139,6 @@ var BilimtekSwarm=function(token,venueId){
 		this.getFoursquareData();
 		this.iteratePeople();
 	}
+	
 	var self = this;
 }
