b7c56d8271d0e5363db55a86040705f86183e266
Oguzhan Duran <oguzhan@bilimtek.com>
2016-02-03 14:07:51 +0200

    HomisManagement new screen yeni geliştirmeler yapıldı

---
+++ Musteriler/raspberry/appconfigs/trilye/appconfig.js
@@ -0,0 +1,11 @@
+﻿var appConfig = 
+{
+	twitterId:"694161607937671169",
+	twitterName:"trilyerestoran",
+	sliderMedia:"twitter",
+	swarmVenueId:"56a8999b498e6d9f0d8add0c",
+	swarmOauthToken:"A5P5WIBWXDOX5PJRMWL3NDI2LVQX2HVJQBAC0CZHMS2RIM15"
+}
+
+
+ 
