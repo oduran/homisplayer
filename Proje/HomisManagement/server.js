7aaf3b1d05d82f6be0e1d402fd4e02d70a91f782
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-02-10 16:50:35 +0200

    require path

--- Proje/HomisManagement/server.js
+++ Proje/HomisManagement/server.js
@@ -3,6 +3,7 @@
 // BASE SETUP
 // =============================================================================
 // call the packages we need
+var path = require("path");
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
