a8c25de0272aad66d644a149cc9635fa9bdda02f
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-02-24 16:17:50 +0200

    Screen image thumbnail resize ediliyor artık! Oğuzhan' a sevgilerle.

--- Proje/HomisManagement/Presentation/script/workspace.js
+++ Proje/HomisManagement/Presentation/script/workspace.js
@@ -584,15 +584,53 @@ var WallManager = function () {
       var iframeHtml = document.getElementById('templateUrl').contentWindow.document.getElementsByTagName("html").outerHTML;
       html2canvas(iframe, {
         onrendered: function(canvas) {
-            // canvas is the final rendered <canvas> element
-           
-         
-          $("#"+wallScreenId).css("background-image","url("+canvas.toDataURL('image/png')+")");
+          // canvas is the final rendered <canvas> element
+           var base64Pic = canvas.toDataURL('image/png');
+           var thumbnailWidth = parseInt(canvas.width/4);
+           var thumbnailHeight = parseInt(canvas.height/4);
+           var thumbnail = imageToThumbnail(base64Pic,thumbnailWidth,thumbnailHeight);
+
+          $("#"+wallScreenId).css("background-image","url("+thumbnail+")");
           $('#screenConfigModal').modal('hide');
            
         }
       });
     });
+  }
+  
+  var imageToThumbnail = function(base64, maxWidth, maxHeight) {
+    // Max size for thumbnail
+    if(typeof(maxWidth) === 'undefined') var maxWidth = 500;
+    if(typeof(maxHeight) === 'undefined') var maxHeight = 500;
+
+    // Create and initialize two canvas
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    var canvasCopy = document.createElement("canvas");
+    var copyContext = canvasCopy.getContext("2d");
+
+    // Create original image
+    var img = new Image();
+    img.src = base64;
+
+    // Determine new ratio based on max size
+    var ratio = 1;
+    if(img.width > maxWidth)
+      ratio = maxWidth / img.width;
+    else if(img.height > maxHeight)
+      ratio = maxHeight / img.height;
+
+    // Draw original image in second canvas
+    canvasCopy.width = img.width;
+    canvasCopy.height = img.height;
+    copyContext.drawImage(img, 0, 0);
+
+    // Copy and resize second canvas to first canvas
+    canvas.width = img.width * ratio;
+    canvas.height = img.height * ratio;
+    ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, canvas.width, canvas.height);
+
+    return canvas.toDataURL();
   }
   
   var addCloseWallScreen = function ()
