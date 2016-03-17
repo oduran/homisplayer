61aa254edda4fe1a654208990a2e55c4c27959ff
Oguzhan Duran <oguzhan@bilimtek.com>
2016-03-17 17:15:18 +0200

    player oynatması walların süresine göre ayarlanmıştır

--- Proje/HomisPlayer/script/player.js
+++ Proje/HomisPlayer/script/player.js
@@ -1,7 +1,11 @@
  
  var Player = function()
  {
-   
+  var walls=[];
+  var wall={};
+  var startTime ;
+  var endTime ;
+  this.date = new Date();
   var regexType = ["css","script","media"];
   var fs = require('fs');
   var fileManager = new FileManager();
@@ -70,46 +74,141 @@
   };
   
   var showWorkspace = function(player)
+  {
+    var workspace = player.workspace;
+    if(workspace)
+    {
+      var walls = workspace.walls;
+      var startTime = workspace.starttime;
+      var endTime = workspace.endtime;
+      var container = document.getElementsByClassName('bilimtekcontainer');
+      var playerDiv = document.createElement("div");
+      playerDiv.style.width = workspace.width+"px";
+      playerDiv.style.height = workspace.height+"px";
+      var timeDifference = calculateTimeDifference(startTime,endTime);
+      var totalSpentTime = 0;
+              
+      var i=0;
+      var showTime;
+      
+      var setScreenTimeoutWalls = function()
       {
-        var workspace = player.workspace;
-        if(workspace)
-        {
-          var container = document.getElementsByClassName('bilimtekcontainer');
-          var playerDiv = document.createElement("div");
-          playerDiv.style.width = workspace.width+"px";
-          playerDiv.style.height = workspace.height+"px";
-          for(var i = 0; i<workspace.walls.length;i++)
+         var wall = workspace.walls[i];
+         showTime = wall.showTime;
+         console.log(showTime);
+         var checkDeterminedTimeInterval = setInterval(function()
           {
-            for (var j = 0 ; j<workspace.walls[i].screens.length;j++ )
+            for(var i = 0 ;i<walls.length;i++)
             {
-              var iframe = document.createElement("iframe");
-              iframe.style.width = workspace.width/workspace.walls[i].screens.length+"px";
-              iframe.style.height = workspace.height+"px";
-              iframe.style.border = "0px";
-              var htmlDoc = workspace.walls[i].screens[j].html;
-              
-              if(htmlDoc.indexOf("http")===0)
-              {
-                iframe.src = htmlDoc;
-                playerDiv.appendChild(iframe);
-              }
-              
-              else
-              {
-               iframe.srcdoc = workspace.walls[i].screens[j].html;  
-               playerDiv.appendChild(iframe);
-               for(var k = 0; k<regexType.length;k++)
-                {
-                  checkContentFunction(iframe.srcdoc,regexType[k]);  
-                  iframe.srcdoc = changeElementsUrl(iframe.srcdoc,regexType[k]);
+                if(walls[i].showTime.indexOf("-")>0)
+                { 
+                   self.date = new Date();
+                   var start=walls[i].showTime.split("-")[0];
+                   var end=walls[i].showTime.split("-")[1];
+                   var dateOfScreen = stringToDate(start);
+                   
+                    if(self.date.getHours()===dateOfScreen.getHours()&&
+                      self.date.getMinutes()===dateOfScreen.getMinutes()&&
+                      self.date.getSeconds()-dateOfScreen.getSeconds()<2)
+                    {
+                     var counter = calculateTimeDifference(start,end); 
+                      
+                     playerDiv = setPlayerWalls(workspace,walls[i],playerDiv);
+                     container[0].innerHTML = "";
+                     container[0].appendChild(playerDiv);
+                     clearInterval(interval);
+                     interval = setInterval(setScreenTimeoutWalls, counter[0]*100*60);
+                    }
                 }
-              }
             }
-          }
-          container[0].innerHTML = "";
-          container[0].appendChild(playerDiv);
+           
+          }, 1000);
+
+         totalSpentTime += parseInt(wall.showTime);
+         playerDiv = setPlayerWalls(workspace,wall,playerDiv);
+         container[0].innerHTML = "";
+         container[0].appendChild(playerDiv);
+         i++;
+
+         if(workspace.walls.length===i)
+         {
+           i=0;
+         }
+
+         if(timeDifference[0]<=totalSpentTime)
+         {
+          totalSpentTime=0;
+         }
+         
+         clearInterval(interval);
+         interval = setInterval(setScreenTimeoutWalls, wall.showTime*100*60);
+     }
+     
+     var interval = setInterval(setScreenTimeoutWalls,wall.showTime*100*60);
+    }
+  };
+  
+  var stringToDate = function(dateString)
+  {
+    var splittedTime = dateString.split(":");
+    var hour = splittedTime[0];
+    var minute = splittedTime[1];
+    var time = new Date(self.date.getFullYear(), self.date.getMonth(),self.date.getDate(),hour,minute,0);
+    return time;
+  };
+  
+  var calculateTimeDifference = function(startTime,endTime)
+  {
+    var time1 = startTime.split(':');
+    var time2 = endTime.split(':');
+    var startHour = parseInt(time1[0], 10); 
+    var endHour = parseInt(time2[0], 10);
+    var startMin = parseInt(time1[1], 10);
+    var endMin = parseInt(time2[1], 10);
+    var hours = endHour - startHour, mins = 0;
+    if(hours < 0) hours = 24 + hours;
+    if(endMin >= startMin) {
+       mins = endMin - startMin;
+    }
+    else {
+       mins = (endMin + 60) - startMin;
+       hours--;
+    }
+    mins = mins / 60; // take percentage in 60
+    hours += mins;
+    return [hours*60,startHour,startMin,endHour,endMin];
+  };
+  
+  var setPlayerWalls = function (workspace,wall,playerDiv)
+  {
+   
+    for (var j = 0 ; j<wall.screens.length;j++ )
+    {
+      var iframe = document.createElement("iframe");
+      iframe.style.width = workspace.width/wall.screens.length+"px";
+      iframe.style.height = workspace.height+"px";
+      iframe.style.border = "0px";
+      var htmlDoc = wall.screens[j].html;
+      playerDiv.innerHTML ="";
+      if(htmlDoc.indexOf("http")===0)
+      {
+        iframe.src = htmlDoc;
+        playerDiv.appendChild(iframe);
+      }
+      
+      else
+      {
+       iframe.srcdoc = wall.screens[j].html; 
+       playerDiv.appendChild(iframe);
+       for(var k = 0; k<regexType.length;k++)
+        {
+          checkContentFunction(iframe.srcdoc,regexType[k]);  
+          iframe.srcdoc = changeElementsUrl(iframe.srcdoc,regexType[k]);
         }
       }
+    }
+    return playerDiv;
+  }
   
   var changeElementsUrl = function(playerDiv,regexType)
   {
