523d70f6ba96292ff7f473656013d0c3e7c78282
onderaltintas@gmail.com <onderaltintas@gmail.com>
2016-03-22 15:33:51 +0200

    Player.js iki ayrı sınıfa bölündü, PlayerUI ve PlayerManager

--- Proje/HomisPlayer/script/playermanager.js
+++ Proje/HomisPlayer/script/playermanager.js
@@ -1,10 +1,12 @@
- 
- var Player = function()
+ var PlayerManager = function()
  {
   var url = "http://www.bilimtek.com:8080";
   url = "http://192.168.2.8:8080";
   var fileManager = new FileManager();
+  var playerUI = new PlayerUI();
   var downloading = false;
+  var timeouts = [];
+  var intervals = [];
   var directories =
   {
     presentation : "presentation",
@@ -16,7 +18,7 @@
     public : "presentation/public"
   };
   
-  this.initializePlayerPage = function()
+  this.initializePlayer = function()
   {
     if(downloading)
     {
@@ -38,99 +40,6 @@
       });  
     }
   };
-  
-  var showWorkspace = function(player)
-  { 
-    var workspace = player.workspace;
-    if(workspace)
-    { 
-      var container = document.getElementsByClassName('bilimtekcontainer');
-      var playerDiv = document.createElement("div");
-      playerDiv.style.width = workspace.width+"px";
-      playerDiv.style.height = workspace.height+"px";
-      var walls = workspace.walls;
-      var startTime = workspace.starttime;
-      var endTime = workspace.endtime;
-      var timeDifference = calculateTimeDifference(startTime,endTime);
-      var totalSpentTime = 0;
-      var i=0;
-      var showTime;
-      var setScreenTimeoutWalls = function()
-      {
-        var newWall = walls[i];
-        showTime = newWall.showTime;
-        var checkDeterminedTimeInterval = setInterval(function()
-        {
-          for(var i = 0 ;i<walls.length;i++)
-          {
-              if(walls[i].showTime.indexOf("-")>0)
-              { 
-                self.date = new Date();
-                var start=walls[i].showTime.split("-")[0];
-                var end=walls[i].showTime.split("-")[1];
-                var dateOfScreen = stringToDate(start);
-                 
-                if(self.date.getHours()===dateOfScreen.getHours()&&
-                  self.date.getMinutes()===dateOfScreen.getMinutes()&&
-                  self.date.getSeconds()-dateOfScreen.getSeconds()<2)
-                {
-                 var counter = calculateTimeDifference(start,end); 
-                 playerDiv = setPlayerWalls(workspace,walls[i],playerDiv);
-                 container[0].innerHTML = "";
-                 container[0].appendChild(playerDiv);
-                 setTimeout(setScreenTimeoutWalls, counter[0]*1000*60);
-                }
-              }
-          }
-        }, 1000);
-        
-        totalSpentTime += parseInt(newWall.showTime);
-        playerDiv = setPlayerWalls(workspace,newWall,playerDiv);
-        container[0].innerHTML = "";
-        container[0].appendChild(playerDiv);
-        i++;
-
-        if(workspace.walls.length===i)
-        {
-          i=0;
-        }
-
-        if(timeDifference[0]<=totalSpentTime)
-        {
-          totalSpentTime=0;
-        }
-
-        setTimeout(setScreenTimeoutWalls, newWall.showTime*1000*60);
-       }
-     
-       setTimeout(setScreenTimeoutWalls,2000);
-    }
-  };
-  
-  var setPlayerWalls = function (workspace,wall,playerDiv)
-  {
-    for (var j = 0 ; j<wall.screens.length;j++ )
-    { 
-      var iframe = document.createElement("iframe");
-      iframe.style.width = workspace.width/wall.screens.length+"px";
-      iframe.style.height = workspace.height+"px";
-      iframe.style.border = "0px";
-      var htmlDoc = wall.screens[j].html;
-      
-      if(htmlDoc.indexOf("http")===0)
-      {
-        iframe.src = htmlDoc;
-        playerDiv.appendChild(iframe);
-      }
-      else
-      {
-        iframe.srcdoc = "<!DOCTYPE HTML>"+htmlDoc;
-        playerDiv.appendChild(iframe);
-      }
-    }
-    
-    return playerDiv;
-  }
   
   var getPlayerId = function()
   {
@@ -159,6 +68,19 @@
       }
       getFiles(fileUrls,player,getFilesSuccess);
     }
+    else if(previousPlayerFile.toString())
+    {
+      for(var i=0;i<previousPlayerFile.workspace.walls.length;i++)
+      {
+        for(var j = 0;j<previousPlayerFile.workspace.walls[i].screens.length;j++)
+        {
+          previousPlayerFile.workspace.walls[i].screens[j].html = changeHtmlUrls(previousPlayerFile.workspace.walls[i].screens[j].html);
+        }
+      }
+      
+      playerUI.showWorkspace(previousPlayerFile);
+    }
+    
   };
   
   var getFilesSuccess = function(files,player)
@@ -185,8 +107,8 @@
           downloadedCssFiles++;
           if(downloadedCssFiles===cssFiles.length)
           {
+            playerUI.showWorkspace(player);  
             downloading = false;
-            showWorkspace(player);  
           }
         });
     }
@@ -235,6 +157,11 @@
     var fileUrls=[];
     var regex = /(href=|src=)\"(.*)(\.css|\.js|\.webm|\.jpeg|\.jpg|\.png)"/g;
     var result = htmlContent.match(regex);
+    if(!result)
+    {
+      return fileUrls;
+    }
+    
     for(var i = 0 ;i<result.length ; i++)
     {
       result[i] = result[i].replace("href=","").replace("src=","").replace(/\"/g,"");
@@ -355,28 +282,6 @@
     }
   };
   
-  var calculateTimeDifference = function(startTime,endTime)
-  {
-    var time1 = startTime.split(':');
-    var time2 = endTime.split(':');
-    var startHour = parseInt(time1[0], 10); 
-    var endHour = parseInt(time2[0], 10);
-    var startMin = parseInt(time1[1], 10);
-    var endMin = parseInt(time2[1], 10);
-    var hours = endHour - startHour, mins = 0;
-    if(hours < 0) hours = 24 + hours;
-    if(endMin >= startMin) {
-       mins = endMin - startMin;
-    }
-    else {
-       mins = (endMin + 60) - startMin;
-       hours--;
-    }
-    mins = mins / 60; // take percentage in 60
-    hours += mins;
-    return [hours*60,startHour,startMin,endHour,endMin];
-  };
-  
   var mergeArrays = function(array1,array2)
   {
     for(var i = 0; i<array2.length;i++)
@@ -392,10 +297,10 @@
 
 $( document ).ready(function() 
 {
-  var player = new Player();
+  var playerManager = new PlayerManager();
   var playerInterval = setInterval(function(){
-    player.initializePlayerPage();
+    playerManager.initializePlayer();
   },60000);
   
-  player.initializePlayerPage();
+  playerManager.initializePlayer();
 });
---
+++ Proje/HomisPlayer/script/playerui.js
@@ -0,0 +1,134 @@
+ var PlayerUI = function()
+ {
+   
+  this.showWorkspace = function(player)
+  { 
+    var workspace = player.workspace;
+
+    if(workspace)
+    { 
+      var container = document.getElementsByClassName('bilimtekcontainer');
+      var playerDiv = document.createElement("div");
+      playerDiv.style.width = workspace.width+"px";
+      playerDiv.style.height = workspace.height+"px";
+      var walls = workspace.walls;
+      var startTime = workspace.starttime;
+      var endTime = workspace.endtime;
+      var timeDifference = calculateTimeDifference(startTime,endTime);
+      var totalSpentTime = 0;
+      var i=0;
+      var showTime;
+      var setScreenTimeoutWalls = function()
+      {
+        var newWall = walls[i];
+        showTime = newWall.showTime;
+        var checkDeterminedTimeInterval = setInterval(function()
+        {
+          for(var i = 0; i< intervals.length; i++)
+          {
+            clearInterval(intervals[i]);
+          }
+          
+          intervals = [];
+          intervals.push(checkDeterminedTimeInterval);
+          for(var i = 0 ;i<walls.length;i++)
+          {
+              if(walls[i].showTime.indexOf("-")>0)
+              { 
+                self.date = new Date();
+                var start=walls[i].showTime.split("-")[0];
+                var end=walls[i].showTime.split("-")[1];
+                var dateOfScreen = stringToDate(start);
+                 
+                if(self.date.getHours()===dateOfScreen.getHours()&&
+                  self.date.getMinutes()===dateOfScreen.getMinutes()&&
+                  self.date.getSeconds()-dateOfScreen.getSeconds()<2)
+                {
+                 var counter = calculateTimeDifference(start,end); 
+                 playerDiv = setPlayerWalls(workspace,walls[i],playerDiv);
+                 container[0].innerHTML = "";
+                 container[0].appendChild(playerDiv);
+                 setTimeout(setScreenTimeoutWalls, counter[0]*1000*60);
+                }
+              }
+          }
+        }, 1000);
+        
+        totalSpentTime += parseInt(newWall.showTime);
+        playerDiv = setPlayerWalls(workspace,newWall,playerDiv);
+        container[0].innerHTML = "";
+        container[0].appendChild(playerDiv);
+        i++;
+
+        if(workspace.walls.length===i)
+        {
+          i=0;
+        }
+
+        if(timeDifference[0]<=totalSpentTime)
+        {
+          totalSpentTime=0;
+        }
+
+        var timeoutId = setTimeout(setScreenTimeoutWalls, newWall.showTime*1000*60);
+        timeouts.push(timeoutId);
+      }
+      for(var i = 0 ; i<timeouts.length;i++)
+      {
+        clearTimeout(timeouts[i]);
+      }
+      
+      timeouts = [];
+      var timeoutId = setTimeout(setScreenTimeoutWalls,2000);
+      timeouts.push(timeoutId);
+    }
+  }; 
+   
+  var setPlayerWalls = function (workspace,wall,playerDiv)
+  {
+    for (var j = 0 ; j<wall.screens.length;j++ )
+    { 
+      var iframe = document.createElement("iframe");
+      iframe.style.width = workspace.width/wall.screens.length+"px";
+      iframe.style.height = workspace.height+"px";
+      iframe.style.border = "0px";
+      var htmlDoc = wall.screens[j].html;
+      
+      if(htmlDoc.indexOf("http")===0)
+      {
+        iframe.src = htmlDoc;
+        playerDiv.appendChild(iframe);
+      }
+      else
+      {
+        iframe.srcdoc = "<!DOCTYPE HTML>"+htmlDoc;
+        playerDiv.appendChild(iframe);
+      }
+    }
+  }
+  
+  var calculateTimeDifference = function(startTime,endTime)
+  {
+    var time1 = startTime.split(':');
+    var time2 = endTime.split(':');
+    var startHour = parseInt(time1[0], 10); 
+    var endHour = parseInt(time2[0], 10);
+    var startMin = parseInt(time1[1], 10);
+    var endMin = parseInt(time2[1], 10);
+    var hours = endHour - startHour, mins = 0;
+    if(hours < 0) hours = 24 + hours;
+    if(endMin >= startMin) {
+       mins = endMin - startMin;
+    }
+    else {
+       mins = (endMin + 60) - startMin;
+       hours--;
+    }
+    mins = mins / 60; // take percentage in 60
+    hours += mins;
+    return [hours*60,startHour,startMin,endHour,endMin];
+  };
+    
+    return playerDiv;
+  }
+ }
